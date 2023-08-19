c4423bb72412aec07810a27cc316d2d2022253f9
DieSeeKat <lukasanthonissen@gmail.com>
2023-08-19 11:00:56 +0200

    Add getters and setters to Chromosome

--- Classes/utils.hpp
+++ Classes/utils.hpp
@@ -18,4 +18,43 @@ class Chromosome
 {
 public:
     deque<Choice> choices;
+
+    Choice getAndSet(int index, Choice choice);
+    Choice get(int index);
+    void set(int index, Choice choice);
+    void add(Choice choice);
+    void remove(int index);
+    int size();
 };
+
+Choice Chromosome::getAndSet(int index, Choice choice)
+{
+    Choice old_choice = this->choices[index];
+    this->choices[index] = choice;
+    return old_choice;
+}
+
+Choice Chromosome::get(int index)
+{
+    return this->choices[index];
+}
+
+void Chromosome::set(int index, Choice choice)
+{
+    this->choices[index] = choice;
+}
+
+void Chromosome::add(Choice choice)
+{
+    this->choices.push_back(choice);
+}
+
+void Chromosome::remove(int index)
+{
+    this->choices.erase(this->choices.begin() + index);
+}
+
+int Chromosome::size()
+{
+    return this->choices.size();
+}
